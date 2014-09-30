7e66bb74a4999cbef45746bffd80cf7c04e017a2
Jane Last <jane.kempton@gmail.com>
2014-09-30 11:12:31 +0100

    Added an encapsulation example

---
+++ encapsulation.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Person
+  attr_accessor :hair_color, :top_color, :height
+  attr_reader :height
+  # Instance Variables
+  def initialize(hair = 'Brown', top = 'Red', height = 0.0)
+    @hair_color = hair
+    @top_color = top
+    @height = height   
+  end
+      
+  def details
+    'Your hair color is ' + @hair_color + '. Your height is ' + @height.to_s + '. Your top colour is ' + @top_color + '.'
+  end   
+  
+  #Instance methods
+   def dance
+    'I am Dancing'
+  end
+  
+  def sleep
+    'I am Sleeping'
+  end
+ 
+binding.pry
+  
+end
+
